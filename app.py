06e3a4cf7edd0f86329735b62e749a7341bae71d
ericluoliu <erikiceliu@gmail.com>
2023-04-16 22:26:13 -0700

    k

--- app.py
+++ app.py
@@ -5,19 +5,43 @@ import random
 import pretty_midi
 import processor
 
+from flask import Flask, jsonify, request
+
 from processor import encode_midi, decode_midi
 
 from utilities.argument_funcs import parse_generate_args, print_generate_args
 from model.music_transformer import MusicTransformer
-from dataset.e_piano import create_epiano_datasets, compute_epiano_accuracy, process_midi
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 
 from utilities.constants import *
 from utilities.device import get_device, use_cuda
+from utilities.device import cpu_device
 
+SEQUENCE_START = 0
+
+OUTPUT_PATH = "../output"
+
+MODEL_WEIGHTS = '../model_lofi/results/best_loss_weights.pickle'
+
+RPR = ""
+
+TARGET_SEQ_LENGTH = 1023
+
+NUM_PRIME = 65
+
+MAX_SEQUENCE = 2048
+
+N_LAYERS = 6
+
+NUM_HEADS = 8
+
+D_MODEL = 512
+
+DIM_FEEDFORWARD = 1024
+    
 # main
-def main():
+def main(primer_midi):
     """
     ----------
     Author: Damon Gwinn
@@ -26,24 +50,23 @@ def main():
     ----------
     """
 
-    args = parse_generate_args()
-    print_generate_args(args)
-
-    if(args.force_cpu):
+    '''if(args.force_cpu):
         use_cuda(False)
         print("WARNING: Forced CPU usage, expect model to perform slower")
-        print("")
+        print("")'''
 
-    os.makedirs(args.output_dir, exist_ok=True)
+    #os.makedirs(OUTPUT_PATH, exist_ok=True)
 
     # Grabbing dataset if needed
-    _, _, dataset = create_epiano_datasets(args.midi_root, args.num_prime, random_seq=False)
+    #_, _, dataset = create_epiano_datasets(args.midi_root, args.num_prime, random_seq=False)
 
     # Can be None, an integer index to dataset, or a file path
-    if(args.primer_file is None):
+    '''if(args.primer_file is None):
         f = str(random.randrange(len(dataset)))
     else:
-        f = args.primer_file
+        f = args.primer_file'''
+
+    '''
 
     if(f.isdigit()):
         idx = int(f)
@@ -52,28 +75,33 @@ def main():
 
         print("Using primer index:", idx, "(", dataset.data_files[idx], ")")
 
-    else:
-        raw_mid = encode_midi(f)
-        if(len(raw_mid) == 0):
-            print("Error: No midi messages in primer file:", f)
-            return
+    '''
 
-        primer, _  = process_midi(raw_mid, args.num_prime, random_seq=False)
-        primer = torch.tensor(primer, dtype=TORCH_LABEL_TYPE, device=get_device())
+    
+    raw_mid = encode_midi(primer_midi)
+    if(len(raw_mid) == 0):
+        print("Error: No midi messages in primer file:", f)
+        return
 
-        print("Using primer file:", f)
+    primer, _  = process_midi(raw_mid, NUM_PRIME, random_seq=False)
+    primer = torch.tensor(primer, dtype=TORCH_LABEL_TYPE, device=get_device())
 
-    model = MusicTransformer(n_layers=args.n_layers, num_heads=args.num_heads,
-                d_model=args.d_model, dim_feedforward=args.dim_feedforward,
-                max_sequence=args.max_sequence, rpr=args.rpr).to(get_device())
+    print("Using primer file:", f)
 
-    model.load_state_dict(torch.load(args.model_weights))
+    model = MusicTransformer(n_layers=N_LAYERS, num_heads=NUM_HEADS,
+                d_model=D_MODEL, dim_feedforward=DIM_FEEDFORWARD,
+                max_sequence=MAX_SEQUENCE, rpr=RPR).to(get_device())
+
+    model.load_state_dict(torch.load(MODEL_WEIGHTS))
 
     # Saving primer first
-    f_path = os.path.join(args.output_dir, "primer.mid")
-    decode_midi(primer[:args.num_prime].cpu().numpy(), file_path=f_path)
+    f_path = os.path.join(OUTPUT_PATH, "primer.mid")
+    decode_midi(primer[:NUM_PRIME].cpu().numpy(), file_path=f_path)
 
-    # GENERATION
+
+
+    # GENERATION 
+    # TODO: do not store the file, return processed midi back to caller
     model.eval()
     with torch.set_grad_enabled(False):
         if(args.beam > 0):
@@ -90,6 +118,52 @@ def main():
             decode_midi(rand_seq[0].cpu().numpy(), file_path=f_path)
 
 
+# process_midi
+def process_midi(raw_mid, max_seq, random_seq):
+    """
+    ----------
+    Author: Damon Gwinn
+    ----------
+    Takes in pre-processed raw midi and returns the input and target. Can use a random sequence or
+    go from the start based on random_seq.
+    ----------
+    """
+
+    x   = torch.full((max_seq, ), TOKEN_PAD, dtype=TORCH_LABEL_TYPE, device=cpu_device())
+    tgt = torch.full((max_seq, ), TOKEN_PAD, dtype=TORCH_LABEL_TYPE, device=cpu_device())
+
+    raw_len     = len(raw_mid)
+    full_seq    = max_seq + 1 # Performing seq2seq
+
+    if(raw_len == 0):
+        return x, tgt
+
+    if(raw_len < full_seq):
+        x[:raw_len]         = raw_mid
+        tgt[:raw_len-1]     = raw_mid[1:]
+        tgt[raw_len]        = TOKEN_END
+    else:
+        # Randomly selecting a range
+        if(random_seq):
+            end_range = raw_len - full_seq
+            start = random.randint(SEQUENCE_START, end_range)
+
+        # Always taking from the start to as far as we can
+        else:
+            start = SEQUENCE_START
+
+        end = start + full_seq
+
+        data = raw_mid[start:end]
+
+        x = data[:max_seq]
+        tgt = data[1:full_seq]
+
+
+    # print("x:",x)
+    # print("tgt:",tgt)
+
+    return x, tgt
 
 
 if __name__ == "__main__":
